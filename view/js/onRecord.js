92948ae0d17c3288cd80cf21508192561bd8878f
Willy-Chen <willychenwii@gmail.com>
2022-01-14 15:06:19 +0000

    front-end feature: 1. Recording feature, added js script for saving recorded file temporary
    to-do:
    1. save recording file to local
    2. make recording text box better, more neat to be exact.

--- view/js/onRecord.js
+++ view/js/onRecord.js
@@ -1,55 +1,180 @@
-// Set up the AudioContext.
-const audioCtx = new AudioContext();
+// set up basic variables for app
 
-// Top-level variable keeps track of whether we are recording or not.
-let recording = false;
+const record = document.querySelector('.record');
+const stop = document.querySelector('.stop');
+const soundClips = document.querySelector('.sound-clips');
+const canvas = document.querySelector('.visualizer');
+const mainSection = document.querySelector('.main-controls');
 
-// Ask user for access to the microphone.
-if (navigator.mediaDevices) {
-  navigator.mediaDevices.getUserMedia({"audio": true}).then((stream) => {
+// disable stop button while not recording
 
-    // Instantiate the media recorder.
+stop.disabled = true;
+
+// visualiser setup - create web audio api context and canvas
+
+let audioCtx;
+const canvasCtx = canvas.getContext("2d");
+
+
+//main block for doing the audio recording
+
+if (navigator.mediaDevices.getUserMedia) {
+  console.log('getUserMedia supported.');
+
+  const constraints = { audio: true };
+  let chunks = [];
+
+  let onSuccess = function(stream) {
     const mediaRecorder = new MediaRecorder(stream);
 
-    // Create a buffer to store the incoming data.
-    let chunks = [];
-    mediaRecorder.ondataavailable = (event) => {
-      chunks.push(event.data);
+    visualize(stream);
+
+    record.onclick = function() {
+      mediaRecorder.start();
+      console.log(mediaRecorder.state);
+      console.log("recorder started");
+      record.style.background = "red";
+
+      stop.disabled = false;
+      record.disabled = true;
     }
 
-    // When you stop the recorder, create a empty audio clip.
-    mediaRecorder.onstop = (event) => {
-      const audio = new Audio();
-      audio.setAttribute("controls", "");
-      $("#sound-clip").append(audio);
-      $("#sound-clip").append("<br />");
+    stop.onclick = function() {
+      mediaRecorder.stop();
+      console.log(mediaRecorder.state);
+      console.log("recorder stopped");
+      record.style.background = "";
+      record.style.color = "";
+      // mediaRecorder.requestData();
 
-      // Combine the audio chunks into a blob, then point the empty audio clip to that blob.
-      const blob = new Blob(chunks, {"type": "audio/ogg; codecs=opus"});
-      audio.src = window.URL.createObjectURL(blob);
+      stop.disabled = true;
+      record.disabled = false;
+    }
 
-      // Clear the `chunks` buffer so that you can record again.
-      chunks = [];
-    };
+    mediaRecorder.onstop = function(e) {
+      console.log("data available after MediaRecorder.stop() called.");
 
-    // Set up event handler for the "Record" button.
-    $("#record").on("click", () => {
-      if (recording) {
-        mediaRecorder.stop();
-        recording = false;
-        $("#record").html("Record");
+      const clipName = prompt('Enter a name for your sound clip?','My unnamed clip');
+
+      const clipContainer = document.createElement('article');
+      const clipLabel = document.createElement('p');
+      const audio = document.createElement('audio');
+      const deleteButton = document.createElement('button');
+
+      clipContainer.classList.add('clip');
+      audio.setAttribute('controls', '');
+      deleteButton.textContent = 'Delete';
+      deleteButton.className = 'delete';
+
+      if(clipName === null) {
+        clipLabel.textContent = 'My unnamed clip';
       } else {
-        mediaRecorder.start();
-        recording = true;
-        $("#record").html("Stop");
+        clipLabel.textContent = clipName;
       }
-    });
 
-  }).catch((err) => {
-    // Throw alert when the browser is unable to access the microphone.
-    alert("Oh no! Your browser cannot access your computer's microphone.\n"+err);
-  });
+      clipContainer.appendChild(audio);
+      clipContainer.appendChild(clipLabel);
+      clipContainer.appendChild(deleteButton);
+      soundClips.appendChild(clipContainer);
+
+      audio.controls = true;
+      const blob = new Blob(chunks, { 'type' : 'audio/ogg; codecs=opus' });
+      chunks = [];
+      const audioURL = window.URL.createObjectURL(blob);
+      audio.src = audioURL;
+      console.log("recorder stopped");
+
+      deleteButton.onclick = function(e) {
+        let evtTgt = e.target;
+        evtTgt.parentNode.parentNode.removeChild(evtTgt.parentNode);
+      }
+
+      clipLabel.onclick = function() {
+        const existingName = clipLabel.textContent;
+        const newClipName = prompt('Enter a new name for your sound clip?');
+        if(newClipName === null) {
+          clipLabel.textContent = existingName;
+        } else {
+          clipLabel.textContent = newClipName;
+        }
+      }
+    }
+
+    mediaRecorder.ondataavailable = function(e) {
+      chunks.push(e.data);
+    }
+  }
+
+  let onError = function(err) {
+    console.log('The following error occured: ' + err);
+  }
+
+  navigator.mediaDevices.getUserMedia(constraints).then(onSuccess, onError);
+
 } else {
-  // Throw alert when the browser cannot access any media devices.
-  alert("Oh no! Your browser cannot access your computer's microphone. Please update your browser.");
+   console.log('getUserMedia not supported on your browser!');
 }
+
+function visualize(stream) {
+  if(!audioCtx) {
+    audioCtx = new AudioContext();
+  }
+
+  const source = audioCtx.createMediaStreamSource(stream);
+
+  const analyser = audioCtx.createAnalyser();
+  analyser.fftSize = 2048;
+  const bufferLength = analyser.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
+
+  source.connect(analyser);
+  //analyser.connect(audioCtx.destination);
+
+  draw()
+
+  function draw() {
+    const WIDTH = canvas.width;
+    const HEIGHT = canvas.height;
+
+    requestAnimationFrame(draw);
+
+    analyser.getByteTimeDomainData(dataArray);
+
+    canvasCtx.fillStyle = 'rgb(80, 206, 135)';
+    canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+    canvasCtx.lineWidth = 2;
+    canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+
+    canvasCtx.beginPath();
+
+    let sliceWidth = WIDTH * 1.0 / bufferLength;
+    let x = 0;
+
+
+    for(let i = 0; i < bufferLength; i++) {
+
+      let v = dataArray[i] / 128.0;
+      let y = v * HEIGHT/2;
+
+      if(i === 0) {
+        canvasCtx.moveTo(x, y);
+      } else {
+        canvasCtx.lineTo(x, y);
+      }
+
+      x += sliceWidth;
+    }
+
+    canvasCtx.lineTo(canvas.width, canvas.height/2);
+    canvasCtx.stroke();
+
+  }
+}
+
+window.onresize = function() {
+  canvas.width = mainSection.offsetWidth;
+}
+
+window.onresize();
+
